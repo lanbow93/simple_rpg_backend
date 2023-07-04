3b4300a273983287921d4b896b12f73ce0e2624c
lanbow93 <the.doctor1501@gmail.com>
2023-07-04 13:58:17 -0500

    Monster Schema created

--- models/monsters.js
+++ models/monsters.js
@@ -1,6 +1,16 @@
 const mongoose = require("./connection")
 
-const monsterSchema = new mongoose.Schema({
+const MonsterSchema = new mongoose.Schema({
     monsterName: String,
-    
+    monsterType: String,
+    weapon: String,
+    armor: String,
+    health: Number,
+    experience: Number,
+    gold: Number,
+    inventory: [String]
 })
+
+const Monsters = mongoose.model("Monsters", MonsterSchema)
+
+module.exports = Monsters
