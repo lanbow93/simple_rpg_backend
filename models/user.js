504c7ad77e4fe43533cf13007fb0a43d8753861d
lanbow93 <the.doctor1501@gmail.com>
2023-07-05 09:54:09 -0500

    User model created

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("./connection")
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+}, {timestamps: true})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
